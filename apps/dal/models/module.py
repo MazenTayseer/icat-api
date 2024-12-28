e568e129322e5aeee1ef33a99f8325bd66a9f8f6
MazenTayseer <maziiin360@yahoo.com>
2024-12-28 20:34:23 +0200

    added module model

---
+++ apps/dal/models/module.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from common.utils import generate_uuid
+
+
+class Module(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=255,
+        default=generate_uuid,
+    )
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        app_label = 'dal'
+
+    def __str__(self):
+        return self.name
