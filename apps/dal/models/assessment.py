44a07f853e83a8d1644ad37af2f6735c8f11e4f8
MazenTayseer <maziiin360@yahoo.com>
2024-12-29 01:57:45 +0200

    added user assessments many to many table

--- apps/dal/models/assessment.py
+++ apps/dal/models/assessment.py
@@ -25,3 +25,39 @@ class Assessment(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UserAssessments(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=255,
+        default=generate_uuid,
+    )
+    user = models.ForeignKey(
+        'custom_auth.User',
+        on_delete=models.CASCADE,
+        related_name='assessments'
+    )
+    assessment = models.ForeignKey(
+        'Assessment',
+        on_delete=models.CASCADE,
+        related_name='users',
+    )
+    score = models.FloatField(blank=False, null=False)
+    trial = models.IntegerField(blank=False, null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        app_label = 'dal'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'assessment', 'trial'],
+                name='unique_user_assessment_trial'
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user.email} - {self.assessment.name} - {self.score}"
