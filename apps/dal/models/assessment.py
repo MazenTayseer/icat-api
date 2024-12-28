68fb6fdcbd28bdee26d36c67cffba0396a221ad3
MazenTayseer <maziiin360@yahoo.com>
2024-12-28 20:45:37 +0200

    added assessments model

---
+++ apps/dal/models/assessment.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+from common.utils import generate_uuid
+
+
+class Assessment(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=255,
+        default=generate_uuid,
+    )
+    name = models.CharField(max_length=255, blank=False, null=False)
+    max_score = models.FloatField(default=100, blank=False, null=False)
+    module = models.ForeignKey(
+        'Module',
+        on_delete=models.CASCADE,
+        related_name='assessments',
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        app_label = 'dal'
+
+    def __str__(self):
+        return self.name
