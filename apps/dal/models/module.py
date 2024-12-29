9bdefa3e0352f4fb6abdabb5b13a4d0e8cf48437
MazenTayseer <maziiin360@yahoo.com>
2024-12-29 02:54:15 +0200

    added userModule many to many table

--- apps/dal/models/module.py
+++ apps/dal/models/module.py
@@ -20,3 +20,37 @@ class Module(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UserModule(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=255,
+        default=generate_uuid,
+    )
+    user = models.ForeignKey(
+        'custom_auth.User',
+        on_delete=models.CASCADE,
+        related_name='modules'
+    )
+    module = models.ForeignKey(
+        'Module',
+        on_delete=models.CASCADE,
+        related_name='users',
+    )
+    completion_percentage = models.FloatField(blank=False, null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        app_label = 'dal'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'module'],
+                name='unique_user_module'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user.email} - {self.module}'
