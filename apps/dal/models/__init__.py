4faacad8113eeac0c0799641aacd38af58803dbd
MazenTayseer <maziiin360@yahoo.com>
2024-12-29 03:00:26 +0200

    added models in __init__

---
+++ apps/dal/models/__init__.py
@@ -0,0 +1,14 @@
+from apps.dal.models.assessment import Assessment, UserAssessments
+from apps.dal.models.leaderboard import Leaderboard
+from apps.dal.models.module import Module, UserModule
+from apps.dal.models.simulation import Simulation, UserSimulation
+
+__all__ = [
+    Assessment,
+    UserAssessments,
+    Module,
+    UserModule,
+    Simulation,
+    UserSimulation,
+    Leaderboard,
+]
