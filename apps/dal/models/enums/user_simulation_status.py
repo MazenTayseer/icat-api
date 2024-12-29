3f9eefd868d0f4cf501e093fd624ef17d59df64c
MazenTayseer <maziiin360@yahoo.com>
2024-12-29 02:57:05 +0200

    added user simulation table

---
+++ apps/dal/models/enums/user_simulation_status.py
@@ -0,0 +1,8 @@
+class UserSimulationStatus:
+    IN_PROGRESS = 'IN_PROGRESS'
+    COMPLETED = 'COMPLETED'
+
+    CHOICES = (
+        (IN_PROGRESS, 'In Progress'),
+        (COMPLETED, 'Completed'),
+    )
--- apps/dal/models/simulation.py
+++ apps/dal/models/simulation.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from apps.dal.models.enums.user_simulation_status import UserSimulationStatus
 from common.utils import generate_uuid
 
 
@@ -24,3 +25,43 @@ class Simulation(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UserSimulation(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=255,
+        default=generate_uuid,
+    )
+    user = models.ForeignKey(
+        'custom_auth.User',
+        on_delete=models.CASCADE,
+        related_name='simulations'
+    )
+    simulation = models.ForeignKey(
+        'Simulation',
+        on_delete=models.CASCADE,
+        related_name='users',
+    )
+    status = models.CharField(
+        choices=UserSimulationStatus.CHOICES,
+        default=UserSimulationStatus.IN_PROGRESS,
+        max_length=255,
+        blank=False,
+        null=False
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        app_label = 'dal'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'simulation'],
+                name='unique_user_simulation'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user.email} - {self.simulation}'
