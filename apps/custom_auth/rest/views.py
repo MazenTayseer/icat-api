6170f2682f9f7d0715bcb951c3624f0245b7b775
Mazen Tayseer <maziiin360@gmail.com>
2025-06-22 03:24:38 +0300

    removed set cookies from sign in

--- apps/custom_auth/rest/views.py
+++ apps/custom_auth/rest/views.py
@@ -39,28 +39,11 @@ class SignInView(APIView):
             access_token = str(refresh.access_token)
             refresh_token = str(refresh)
 
-            response = Response({
+            return Response({
                 'message': 'Login successful',
                 'access': access_token,
+                'refresh': refresh_token,
             }, status=status.HTTP_200_OK)
-
-            response.set_cookie(
-                key='refreshToken',
-                value=refresh_token,
-                httponly=True,  # Prevent XSS
-                secure=False,  # Allow HTTP for mixed protocol setup
-                samesite='None',  # Required for cross-site cookies
-                max_age=settings.REFRESH_TOKEN_LIFETIME,
-            )
-            response.set_cookie(
-                key='token',
-                value=access_token,
-                httponly=True,  # Prevent XSS
-                secure=False,  # Allow HTTP for mixed protocol setup
-                samesite='None',  # Required for cross-site cookies
-                max_age=settings.ACCESS_TOKEN_LIFETIME,
-            )
-            return response
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
